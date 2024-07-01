9f35a6ede9678d5c9abf396bf9f6a19d1c8ce0e0
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2024-07-01 15:22:59 -0700

    display spinner & popular tv shows

--- js/script.js
+++ js/script.js
@@ -26,10 +26,36 @@ async function displayPopularMovies() {
 	});
 }
 
+async function displayPopularShows() {
+	const shows = document.querySelector('#popular-shows');
+	const { results } = await fetchAPIData('tv/popular');
+	results.forEach((show) => {
+		const div = document.createElement('div');
+		div.classList.add('card');
+		div.innerHTML = `
+        <a href="tv-details.html?id=${show.id}">
+			${
+				show.poster_path
+					? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" class="card-img-top" alt="${show.name}" />`
+					: `<img src="images/no-image.jpg" class="card-img-top" alt="${show.name}" />`
+			}
+		</a>
+		<div class="card-body">
+			<h5 class="card-title">${show.name}</h5>
+			<p class="card-text">
+				<small class="text-muted">Release: ${show.first_air_date}</small>
+			</p>
+		</div>`;
+		shows.appendChild(div);
+	});
+}
+
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
+	// Store key and make requests from a server
 	const API_KEY = '499feb17664aeae6c821e544e90dec3f';
 	const API_URL = 'https://api.themoviedb.org/3/';
+	showSpinner();
 
 	const response = await fetch(
 		`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
@@ -37,7 +63,17 @@ async function fetchAPIData(endpoint) {
 
 	const data = await response.json();
 
+	hideSpinner();
+
 	return data;
+}
+
+function showSpinner() {
+	document.querySelector('.spinner').classList.add('show');
+}
+
+function hideSpinner() {
+	document.querySelector('.spinner').classList.remove('show');
 }
 
 // Highlight active link
@@ -58,7 +94,7 @@ function init() {
 			displayPopularMovies();
 			break;
 		case '/shows.html':
-			console.log('Shows');
+			displayPopularShows();
 			break;
 		case '/movie-details.html':
 			console.log('Movie details');
