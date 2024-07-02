0d5576a57c2eef4bd3663116d33e246aaaef04d2
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2024-07-02 13:14:15 -0700

    search function & custom alert

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 const global = {
 	currentPage: window.location.pathname,
+	search: {
+		term: '',
+		type: '',
+		page: 1,
+		totalPages: 1,
+	},
+	api: {
+		apiKey: '499feb17664aeae6c821e544e90dec3f',
+		apiUrl: 'https://api.themoviedb.org/3/',
+	},
 };
 
 async function displayPopularMovies() {
@@ -192,6 +202,38 @@ function displayBackgroundImage(type, backgroundPath) {
 	}
 }
 
+// Make Request To Search
+async function searchAPIData() {
+	const API_KEY = global.api.apiKey;
+	const API_URL = global.api.apiUrl;
+	showSpinner();
+
+	const response = await fetch(
+		`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+	);
+
+	const data = await response.json();
+
+	hideSpinner();
+
+	return data;
+}
+
+// Search Movies/Shows
+async function search() {
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	global.search.type = urlParams.get('type');
+	global.search.term = urlParams.get('search-term');
+
+	if (global.search.term !== '' && global.search.term !== null) {
+		const results = await searchAPIData();
+		console.log(results);
+	} else {
+		showAlert('Please enter a search term');
+	}
+}
+
 // Display Slider Movies
 async function displaySlider() {
 	const { results } = await fetchAPIData('movie/now_playing');
@@ -242,8 +284,8 @@ function initSwiper() {
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
 	// Store key and make requests from a server
-	const API_KEY = '499feb17664aeae6c821e544e90dec3f';
-	const API_URL = 'https://api.themoviedb.org/3/';
+	const API_KEY = global.api.apiKey;
+	const API_URL = global.api.apiUrl;
 	showSpinner();
 
 	const response = await fetch(
@@ -275,6 +317,16 @@ function highlightActiveLink() {
 	});
 }
 
+// Show alert
+function showAlert(message, className) {
+	const alertEl = document.createElement('div');
+	alertEl.classList.add('alert', className);
+	alertEl.appendChild(document.createTextNode(message));
+	document.querySelector('#alert').appendChild(alertEl);
+
+	setTimeout(() => alertEl.remove(), 3000);
+}
+
 function addCommasToNumber(number) {
 	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
@@ -297,7 +349,7 @@ function init() {
 			displayShowDetails();
 			break;
 		case '/search.html':
-			console.log('Search');
+			search();
 			break;
 	}
 	highlightActiveLink();
